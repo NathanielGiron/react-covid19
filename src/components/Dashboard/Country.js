53d6342745f45db890a47da90b4be5f5b02209be
Nathaniel Giron <nathaniel.giron@gmail.com>
2020-03-30 12:12:56 -0700

    added new cases/deaths on mobile version

--- src/components/Dashboard/Country.js
+++ src/components/Dashboard/Country.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Country = ({ country, cases, new_cases, deaths, new_deaths, recovered, critical}) => ( 
   <tr>
     <td><strong>{country}</strong></td>
-    <td className="text-danger">{cases}</td>
+    <td className="text-danger"><small className="d-block d-sm-none">{'+' + new_cases}</small>{cases}</td>
     <td className="text-muted d-none d-sm-table-cell">{new_cases === '0' ? '' : '+' + new_cases}</td>
-    <td>{deaths}</td>
+    <td><small className="d-block d-sm-none">{'+' + new_deaths}</small>{deaths}</td>
     <td className="text-muted d-none d-sm-table-cell">{new_deaths === '0' ? '' : '+' + new_deaths}</td>
     <td className="text-info d-none d-sm-table-cell">{ (parseFloat(deaths.split(",").join("")) / parseFloat(cases.split(",").join("")) * 100).toFixed(1) }%</td>
     <td className="text-success d-none d-sm-table-cell">{recovered}</td>
